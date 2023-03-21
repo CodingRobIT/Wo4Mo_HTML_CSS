1f317a6a8514f9cd48cfcd4880d7a7d5f520a6aa
Robin Schatzl <tmai02476@gmail.com>
2023-03-21 10:38:30 +0100

    added js-basics.js but also a new projekt with JS Basics

---
+++ js-basics.js
@@ -0,0 +1,27 @@
+
+const valueA = "Hello World" //const steht für Konstande und ist nicht änderbar
+const valueB = 42
+const valueC = true
+
+//System.ou.println
+console.log(valueA)
+console.log(valueB)
+
+let valueD = "Test"   //let ist änderbar
+console.log(valueD)
+valueD = "Test2"        //wird hier einfach geändert
+console.log(valueD)
+valueD = 4421           //kann auch in einen "integer" einfach so gändert werden daher nicht Typ sicher
+console.log(valueD)
+
+//in Java z.B public void add(int a, int b) {
+//
+//  }
+
+function add(a, b) {
+    return a + b
+}
+console.log(add(3, 8)) //add für "+" und dann wert a + b muss natürlich ein wert eingegeben werden
+
+let value1 = 5
+
